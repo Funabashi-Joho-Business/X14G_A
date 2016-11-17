a118c0ef9f203e6c7fc96f05f4eb4ab76ea3900f
x14g023 <x14g023@chiba-fjb.ac.jp>
2016-11-17 10:50:14 +0900

    配列に格納

--- app/src/main/java/jp/ac/chiba_fjb/example/googlescript/MainActivity.java
+++ app/src/main/java/jp/ac/chiba_fjb/example/googlescript/MainActivity.java
@@ -6,6 +6,9 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jp.ac.chiba_fjb.example.googlescript.Fragment.TopFragment;
 
 
@@ -18,17 +21,110 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.mainLayout,new TopFragment(),TopFragment.class.getName());
+        ft.replace(R.id.mainLayout, new TopFragment(), TopFragment.class.getName());
         ft.addToBackStack(null);
         ft.commit();
     }
+
     @Override
     public void onBackPressed() {
         int backStackCnt = getSupportFragmentManager().getBackStackEntryCount();
         if (backStackCnt != 0) {
             getSupportFragmentManager().popBackStack();
-        }
-        else
+        } else
             super.onBackPressed();
+    }
+
+    public static void anserSend(List<Boolean> numbers, List<Boolean> ansers) {
+        ArrayList<String> sendData = new ArrayList<String>();
+
+        for (int j = 0; j < ansers.size(); j = j + 10) {//ansersの処理
+            for (int i = 0; i < 10; i++) {
+                if (ansers.get(i + j) == true) {
+                    switch (i) {
+                        case 0:
+                            sendData.add("ア");
+                            break;
+                        case 1:
+                            sendData.add("イ");
+                            break;
+                        case 2:
+                            sendData.add("ウ");
+                            break;
+                        case 3:
+                            sendData.add("エ");
+                            break;
+                        case 4:
+                            sendData.add("オ");
+                            break;
+                        case 5:
+                            sendData.add("カ");
+                            break;
+                        case 6:
+                            sendData.add("キ");
+                            break;
+                        case 7:
+                            sendData.add("ク");
+                            break;
+                        case 8:
+                            sendData.add("ケ");
+                            break;
+                        case 9:
+                            sendData.add("コ");
+                            break;
+
+                    }
+                } else {
+                    if (i == 9) {//すべて空白
+                        sendData.add(" ");
+                        break;
+                    }
+                }
+            }
+        }
+
+        //試験番号格納変数
+        String s1 = "";
+        String s2 = "";
+        String s3 = "";
+        //学籍番号格納変数
+        String g1 = "";
+        String g2 = "";
+        String g3 = "";
+        String g4 = "";
+        String g5 = "";
+        String g6 = "";
+
+        for (int j = 0; j < 100; j = j + 10) {
+            for (int i = 0; i < 10; i++) {//試験番号、学籍番号の取得 i=横座標　j=縦座標
+                if (numbers.get(i) == true) {
+                    int a = j / 10 + 1;
+                    if (a == 10)
+                        a = 0;
+                    switch (i) {
+                        case 0:
+                            s1 = String.valueOf(a);
+                        case 1:
+                            s2 = String.valueOf(a);
+                        case 2:
+                            s3 = String.valueOf(a);
+                        case 4:
+                            g1 = String.valueOf(a);
+                        case 5:
+                            g2 = String.valueOf(a);
+                        case 6:
+                            g3 = String.valueOf(a);
+                        case 7:
+                            g4 = String.valueOf(a);
+                        case 8:
+                            g5 = String.valueOf(a);
+                        case 9:
+                            g6 = String.valueOf(a);
+
+                    }
+                }
+
+            }
+        }
     }
 }
