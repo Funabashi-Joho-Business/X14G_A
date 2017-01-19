fa90f6acbb4ace856c425911aa5523aadc3fbac9
Akihiko Oikawa <oikawa@chiba-fjb.ac.jp>
2017-01-19 12:08:24 +0900

    スクリプトライブラリを入れ替え

--- app/src/main/java/jp/ac/chiba_fjb/example/googlescript/GoogleScript.java
+++ app/src/main/java/jp/ac/chiba_fjb/example/googlescript/GoogleScript.java
@@ -1,37 +1,26 @@
 package jp.ac.chiba_fjb.example.googlescript;
 
 
-import android.accounts.AccountManager;
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
-import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
-import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.ExponentialBackOff;
 import com.google.api.services.script.Script;
 import com.google.api.services.script.model.ExecutionRequest;
 import com.google.api.services.script.model.Operation;
 
-import java.util.Arrays;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 
-public class GoogleScript
+public class GoogleScript extends GoogleAccount
 {
-
-
-
 	public static interface ScriptListener{
 		public void onExecuted(GoogleScript script, Operation op);
 	}
@@ -42,23 +31,18 @@ public class GoogleScript
 		public ScriptListener listener;
 	}
 
-	private static final int REQUEST_ACCOUNT_PICKER = 1234;
-	private static final int REQUEST_AUTHORIZATION = 1235;
-	private static final String EXTRA_NAME = "SCRIPT_INFO";
-	private static final String PREF_ACCOUNT_NAME = "ScriptUser";
-	private static final String[] SCOPES = {
-			"https://www.googleapis.com/auth/drive"};
+
+
+	private static final String[] SCOPES = {"https://www.googleapis.com/auth/drive"};
 	private Set<ScriptInfo> mScripts = new HashSet<>();
 	private Activity mContext;
-	private GoogleAccountCredential mCredential;
-	private String mAccountName;
 	private Script mService;
 
 	private static HttpRequestInitializer setHttpTimeout(final HttpRequestInitializer requestInitializer) {
 		return new HttpRequestInitializer() {
 			@Override
 			public void initialize(HttpRequest httpRequest)
-					throws java.io.IOException {
+					throws IOException {
 				requestInitializer.initialize(httpRequest);
 				httpRequest.setReadTimeout(380000);
 			}
@@ -66,59 +50,11 @@ public class GoogleScript
 	}
 
 	public GoogleScript(Activity activity, String[] scope) {
+		super(activity,scope);
 		//Activityの保存
 		mContext = activity;
-		//認証用クラスの生成
-		mCredential = GoogleAccountCredential.usingOAuth2(
-				activity, Arrays.asList(scope==null?SCOPES:scope))
-				.setBackOff(new ExponentialBackOff());
-		//登録済みアカウント名を取得
-		mAccountName = mContext.getPreferences(Context.MODE_PRIVATE).getString(PREF_ACCOUNT_NAME, null);
-	}
-	public void resetAccount()	{
-		//登録アカウントの解除
-		SharedPreferences settings =
-				mContext.getPreferences(Context.MODE_PRIVATE);
-		SharedPreferences.Editor editor = settings.edit();
-		editor.putString(PREF_ACCOUNT_NAME, null);
-		editor.apply();
-		mAccountName = null;
 	}
 
-	public void onActivityResult(int requestCode, int resultCode, Intent data){
-		if(requestCode == REQUEST_ACCOUNT_PICKER) {
-			if (resultCode == Activity.RESULT_OK && data != null && data.getExtras() != null) {
-				mAccountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-
-				if (mAccountName != null) {
-					//アカウント選択確定
-					mCredential.setSelectedAccountName(mAccountName);
-					SharedPreferences settings =
-							mContext.getPreferences(Context.MODE_PRIVATE);
-					SharedPreferences.Editor editor = settings.edit();
-					editor.putString(PREF_ACCOUNT_NAME, mAccountName);
-					editor.apply();
-				}else{
-					requestAccount();
-				}
-
-				exec();		//実行要求
-			}
-			else
-
-				execError();	//実行不能時の処理
-		}
-		else if(requestCode == REQUEST_AUTHORIZATION) {
-			if (resultCode == Activity.RESULT_OK)
-				exec();			//実行要求
-			else
-				execError();	//実行不能時の処理
-		}
-	}
-	public void requestAccount(){
-		//ユーザ選択
-		mContext.startActivityForResult(mCredential.newChooseAccountIntent(),REQUEST_ACCOUNT_PICKER);
-	}
 	public void execute(String scriptId, String name, List<Object> params, ScriptListener listener){
 		//実行に必要な情報を保存
 		ScriptInfo info = new ScriptInfo();
@@ -128,85 +64,42 @@ public class GoogleScript
 		info.listener = listener;
 		mScripts.add(info);
 
-		if(mAccountName == null){
-			//名前選択を呼び出す
-			mContext.startActivityForResult(mCredential.newChooseAccountIntent(),REQUEST_ACCOUNT_PICKER);
-		}else{
-			//名前を確定し実行
-			mCredential.setSelectedAccountName(mAccountName);
-			exec();
-		}
+		//サーバに要求開始
+		call();
 	}
-	void execError() {
+
+	protected void onExec() throws IOException {
+		HttpTransport transport = AndroidHttp.newCompatibleTransport();
+		JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+		mService = new Script.Builder(
+				transport, jsonFactory, setHttpTimeout(getCredential()))
+				.setApplicationName("Google Apps Script Execution API")
+				.build();
+
+		for(final ScriptInfo info : mScripts) {
+			ExecutionRequest request = new ExecutionRequest().setFunction(info.functionName);
+			if (info.params != null)
+				request.setParameters(info.params);
+
+			final Operation op = mService.scripts().run(info.scriptId, request).execute();
+			if (info.listener != null) {
+				mContext.runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						info.listener.onExecuted(GoogleScript.this, op);
+					}
+				});
+			}
+
+		}
+		mScripts.clear();
+	}
+	protected void onError() {
 		//エラーを通知し、実行キューを解除
 		for(ScriptInfo info : mScripts) {
 			if(info.listener != null)
 				info.listener.onExecuted(GoogleScript.this,null);
 			mScripts.remove(info);
-		}
-	}
-	void exec(){
-		HttpTransport transport = AndroidHttp.newCompatibleTransport();
-		JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-		mService = new Script.Builder(
-				transport, jsonFactory, setHttpTimeout(mCredential))
-				.setApplicationName("Google Apps Script Execution API")
-				.build();
-
-		//スレッドで実行
-		for(final ScriptInfo info : mScripts){
-			new Thread() {
-				@Override
-				public void run() {
-					super.run();
-
-					try {
-						ExecutionRequest request = new ExecutionRequest().setFunction(info.functionName);
-						if(info.params != null)
-							request.setParameters(info.params);
-						final Operation op = mService.scripts().run(info.scriptId, request).execute();
-						mScripts.remove(info);
-						if(info.listener != null) {
-							mContext.runOnUiThread(new Runnable() {
-								@Override
-								public void run() {
-									info.listener.onExecuted(GoogleScript.this, op);
-								}
-							});
-						}
-
-					} catch (Exception e) {
-						if (e instanceof UserRecoverableAuthIOException) {
-							//権限要求の呼び出し
-							Intent intent = ((UserRecoverableAuthIOException) e).getIntent();
-							mContext.startActivityForResult(intent,REQUEST_AUTHORIZATION);
-						}
-						else if(e instanceof IllegalArgumentException){
-							//アカウント要求
-							requestAccount();
-						}else if(e instanceof GoogleJsonResponseException){
-							//サーバー登録ミス
-							mScripts.remove(info);
-							if(info.listener != null)
-								mContext.runOnUiThread(new Runnable() {
-									@Override
-									public void run() {
-										info.listener.onExecuted(GoogleScript.this,null);
-									}
-								});
-
-
-						}
-						else {
-							//登録系エラー
-							mScripts.remove(info);
-							if(info.listener != null)
-								info.listener.onExecuted(GoogleScript.this,null);
-							e.printStackTrace();
-						}
-					}
-				}
-			}.start();
 		}
 	}
 }
